51d6516cf64d5efc93da366e4f9f6adae6080d90
Alexander Blume <alex.gos90@gmail.com>
2022-02-24 13:18:39 +0100

    snakefile: update samtools filtering flags

--- snakefile.py
+++ snakefile.py
@@ -293,18 +293,22 @@ rule bwa_align:
     log: os.path.join(LOG_DIR, 'bwa_align_{sample}.log')
     shell: "{BWA_EXEC} mem -t {params.threads} {input.ref} {input.fastq} > {output} 2>> {log} 3>&2"
 
+# TODO verify that subsequent tools do not require filtering for proper pairs
+# NOTE verification of flags can be done with "samtools view -h -f 4 <file.sam|file.bam> | samtools flagstat -
 rule samtools_filter_aligned:
     input: os.path.join(MAPPED_READS_DIR, '{sample}_aligned_tmp.sam')
     output: os.path.join(MAPPED_READS_DIR, '{sample}_aligned.bam')
     log: os.path.join(LOG_DIR, 'samtools_filter_aligned_{sample}.log')
-    shell: "{SAMTOOLS_EXEC} view -bh -f 2 -F 2048 {input} > {output} 2>> {log} 3>&2"
+    shell: # exclude (F) reads that are not mapped (4) and supplementary (2048)
+        "{SAMTOOLS_EXEC} view -bh -F 2052 {input} > {output} 2>> {log} 3>&2"
+
 
 rule samtools_filter_unaligned:
     input: os.path.join(MAPPED_READS_DIR, '{sample}_aligned_tmp.sam')
     output: os.path.join(MAPPED_READS_DIR, '{sample}_unaligned.bam')
     log: os.path.join(LOG_DIR, 'samtools_filter_unaligned_{sample}.log')
-    shell: "{SAMTOOLS_EXEC} view -bh -F 2 {input} > {output} 2>> {log} 3>&2"
-
+    shell: # keep (-f) reads that are unmapped (4)
+        "{SAMTOOLS_EXEC} view -bh -f 4 {input} > {output} 2>> {log} 3>&2"
 
 rule samtools_sort_preprimertrim:
     input: os.path.join(MAPPED_READS_DIR, '{sample}_aligned.bam')
