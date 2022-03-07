c91e2bca960a84df6547143395a28e75325256a5
Alexander Blume <alex.gos90@gmail.com>
2022-03-07 14:43:26 +0100

    snakefile: separate flags for samtools filter
    - instead of using the sum of flags

--- snakefile.py
+++ snakefile.py
@@ -299,8 +299,7 @@ rule samtools_filter_aligned:
     output: os.path.join(MAPPED_READS_DIR, '{sample}_aligned.bam')
     log: os.path.join(LOG_DIR, 'samtools_filter_aligned_{sample}.log')
     shell: # exclude (F) reads that are not mapped (4) and supplementary (2048)
-        "{SAMTOOLS_EXEC} view -bh -F 2052 {input} > {output} 2>> {log} 3>&2"
-
+        "{SAMTOOLS_EXEC} view -bh -F 4 -F 2048 {input} > {output} 2>> {log} 3>&2"
 
 rule samtools_filter_unaligned:
     input: os.path.join(MAPPED_READS_DIR, '{sample}_aligned_tmp.sam')
