afaec2f3d3bec6ada33db0cff7a4893c0131ad26
Alexander Blume <alex.gos90@gmail.com>
2022-04-13 14:07:55 +0200

    creating_variants_summary_table.R: rewrite while loop
    into a vectorized binary function with Reduce and lapply

--- scripts/creating_variants_summary_table.R
+++ scripts/creating_variants_summary_table.R
@@ -1,8 +1,4 @@
 
-
-library(dplyr)
-library(stringr)
-
 get_files <- function ( variants_dir){
   files <- list.files(path = variants_dir,
                     pattern = "_variants.csv",
@@ -13,35 +9,26 @@ get_files <- function ( variants_dir){
 
 create_summary <- function ( files, output_file ){
 
-  require(dplyr)
-
   cat(paste("Summarizing", length(files), "variant files."))
-  # TODO: turn this while loop into a vectorized binary function with Reduce and lapply
-
-  i <- 0
-  while ( i != length(files)){
-
-    if ( file.exists (output_file) ) {
-      df1 <- read.table( output_file,
-                         sep = "\t", header = TRUE, colClasses = "character", check.names = FALSE )
-    } else {
-      df1 <- read.table(files[1],
-                        sep = "\t", header = TRUE, colClasses = "character", check.names = FALSE)
-        # write to output file
-      write.table(df1, output_file, sep = "\t",
-          row.names = FALSE, quote = FALSE)
-      i <- i + 1
-    }
-
-    i <- i + 1
-    df2 <- read.table(files[i],
-                      sep = "\t", header = TRUE, colClasses = "character", check.names = FALSE)
-    output_df <- dplyr::full_join(df1, df2, by = intersect(colnames(df1),
-                                                    colnames(df2)), copy = TRUE)
     # write to output file
     write.table(output_df, output_file, sep = "\t",
           row.names = FALSE, quote = FALSE)
   }
+  # read files into list
+  variants_list <- lapply(X = files,
+                          FUN = read.table,
+                          sep = "\t",
+                          header = TRUE,
+                          colClasses = "character",
+                          check.names = FALSE )
+
+  # merge variant files in pairs
+  merged_variants <- Reduce(f = function(df1,df2) merge(df1,
+                                                    df2,
+                                                    by = intersect(colnames(df1),colnames(df2)),
+                                                    all.x = TRUE ,
+                                                    all.y = TRUE),
+                            x = variants_list)
 }
 
 args <- commandArgs (trailingOnly=TRUE)
