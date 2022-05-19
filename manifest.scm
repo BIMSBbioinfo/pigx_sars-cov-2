41d990d204f1076e93fd1c756a3f92a8d3054f60
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-05-19 15:47:59 +0200

    attempt to fix indentation in manifest.scm

--- manifest.scm
+++ manifest.scm
@@ -21,16 +21,16 @@
 
 (define channels
   (list (channel
-        (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
-        (branch "master")
-        (commit
-          "1c4241d91515b75d4dab160be6b2ad840ceb6335")
-        (introduction
-          (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-            (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))))
+          (name 'guix)
+          (url "https://git.savannah.gnu.org/git/guix.git")
+          (branch "master")
+          (commit
+            "1c4241d91515b75d4dab160be6b2ad840ceb6335")
+          (introduction
+            (make-channel-introduction
+              "9edb3f66fd807b096b48283debdcddccfea34bad"
+                (openpgp-fingerprint
+                  "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))))
 
 (define (lookup name)
   (specification->package name))
