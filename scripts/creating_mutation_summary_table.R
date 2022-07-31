9fc40f70eeae8cc8f94a8a6728a75cfbb9e12697
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-07-31 15:35:44 +0200

    style summary table scripts

--- scripts/creating_mutation_summary_table.R
+++ scripts/creating_mutation_summary_table.R
@@ -1,14 +1,15 @@
-
 create_summary <- function(files) {
   cat(paste("Summarizing", length(files), "mutation files.\n"))
 
   # read files into list
-  mutations_list <- lapply(X = files,
-                          FUN = read.table,
-                          sep = "\t",
-                          header = TRUE,
-                          colClasses = "character",
-                          check.names = FALSE )
+  mutations_list <- lapply(
+    X = files,
+    FUN = read.table,
+    sep = "\t",
+    header = TRUE,
+    colClasses = "character",
+    check.names = FALSE
+  )
 
   # merge mutation files in pairs
   merged_mutations <- Reduce(
@@ -33,7 +34,7 @@ cat(paste(args, collapse = "\",\n\""))
 cat("\"\n\n")
 
 output_file <- args[1]
-files       <- args[2:length(args)]
+files <- args[2:length(args)]
 
 output <- create_summary(files)
 
