a0d4e47ad7bbee47058c7742cdb72e0445ef178f
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-08-01 11:17:48 +0200

    format deconvolution funs deconv*

--- scripts/deconvolution_funs.R
+++ scripts/deconvolution_funs.R
@@ -352,39 +352,44 @@ dedupeVariants <- function (variant, variants.df, dedup_variants.df) {
         return ( list(dedup_variants.df, variants_to_drop) )
 }  
 
-deconv <- function (bulk,sig){
-  #' This function performs the deconvolution using a signature matrix for the mutations found in the sample 
-  #' and bulk frequency values derived by the SNV caller
+
+deconv <- function(bulk, sig) {
+  #' This function performs the deconvolution using a signature matrix for the
+  #' mutations found in the sample and bulk frequency values derived by the SNV
+  #' caller
   #' it was build by Altuna
-    
-  rlm_model = suppressWarnings(MASS::rlm(sig,bulk, maxit = 100, method = "M"))
-      
-      
-  rlm_coefficients = rlm_model$coefficients
-  
-  rlm_coefficients = ifelse(rlm_coefficients < 0, 0, rlm_coefficients)
-  
-  sumOfCof = sum(rlm_coefficients)
-  
-  rlm_coefficients = rlm_coefficients / sumOfCof  #normalize so coefficients add to 1
-  
-  as.vector(rlm_coefficients)
+
+  rlm_model <- suppressWarnings(MASS::rlm(sig, bulk, maxit = 100, method = "M"))
+
+
+  rlm_coefficients <- rlm_model$coefficients
+
+  rlm_coefficients <- ifelse(rlm_coefficients < 0, 0, rlm_coefficients)
+
+  sum_of_cof <- sum(rlm_coefficients)
+
+  # normalize so coefficients add to 1
+  rlm_coefficients <- rlm_coefficients / sum_of_cof
+
+  return(rlm_coefficients)
 }
 
-deconv_debug <- function (bulk,sig){
-  #' This function performs the deconvolution using a signature matrix for the mutations found in the sample
-  #' and bulk frequency values derived by the SNV caller
+deconv_debug <- function(bulk, sig) {
+  #' This function performs the deconvolution using a signature matrix for the
+  #' mutations found in the sample and bulk frequency values derived by the SNV
+  #' caller
   #' it was build by Altuna
 
-  rlm_model = suppressWarnings(MASS::rlm(sig,bulk, maxit = 100, method = "M"))
-    
-  rlm_coefficients = rlm_model$coefficients
+  rlm_model <- suppressWarnings(MASS::rlm(sig, bulk, maxit = 100, method = "M"))
 
-  rlm_coefficients = ifelse(rlm_coefficients < 0, 0, rlm_coefficients)
+  rlm_coefficients <- rlm_model$coefficients
 
-  sumOfCof = sum(rlm_coefficients)
+  rlm_coefficients <- ifelse(rlm_coefficients < 0, 0, rlm_coefficients)
 
-  rlm_coefficients = rlm_coefficients / sumOfCof  #normalize so coefficients add to 1
+  sum_of_cof <- sum(rlm_coefficients)
+
+  # normalize so coefficients add to 1
+  rlm_coefficients <- rlm_coefficients / sum_of_cof
 
   return(list(as.vector(rlm_coefficients), rlm_model$fitted.values))
 }
