5a968ee54a803979d03029152da49157615acd7b
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-09-16 16:01:53 +0200

    add outline of kraken db dl rulescript
      This still needs to be extended. dl url, and downsampling params are
      still hardcoded.

---
+++ snakefile_scripts/rule_download_kraken_db.py
@@ -0,0 +1,108 @@
+import os
+import sys
+import shutil
+import requests
+import tarfile
+import subprocess
+from snakemake.logging import logger
+
+with open(snakemake.log[0], "w") as log_file:
+    sys.stdout = log_file
+    sys.stderr = sys.stdout
+
+    try:
+        logger.info(
+            f"Creating database dir at {snakemake.output}...")
+
+        kraken_dir = snakemake.output[0]
+        os.makedirs(kraken_dir)
+
+        # large db, smaller version is at
+        # db_url = "https://genome-idx.s3.amazonaws.com/kraken/k2_pluspfp_8gb_20210127.tar.gz"
+        # db_url = "https://genome-idx.s3.amazonaws.com/kraken/k2_pluspfp_20210127.tar.gz"
+        db_url = "https://genome-idx.s3.amazonaws.com/kraken/k2_viral_20220607.tar.gz"
+        
+        logger.info(
+            f"Downloading database archive from {db_url}...")
+        # NOTE: This command will download a very large file, after unpacking this will
+        # require about 100GB of disc space. If this is not feasible use another
+        # database instead. For this please see link above.
+        #@WGET@ -qO-  | tar -C $DBNAME -xzv
+        dl_resp = requests.get(db_url, stream = True)
+
+        if not dl_resp.status_code == 200:
+            logger.error(
+                f"Download not successfull, status code {dl_resp.status_code}")
+            sys.exit(1)
+
+        tar_out = tarfile.open(fileobj = dl_resp.raw, mode = "r:gz")
+
+        downsample_db = False
+        max_db_size = 250000000
+
+        if downsample_db:
+            logger.info(
+                f"Downsampling database to {max_db_size}B. "
+                f"This requires additional work.")
+
+            for member in tar_out:
+                if member.name != "hash.k2d":
+                    tar_out.extract(member, path = kraken_dir)
+
+            taxonomy_dl_call = [
+                snakemake.params["kraken2_build"],
+                "--use-ftp",
+                "--download-taxonomy",
+                "--skip-map",
+                "--db", kraken_dir]
+
+            logger.info(
+                "Downloading taxonomy..."
+            )
+
+            logger.info(taxonomy_dl_call)
+        
+            subprocess.run(
+                taxonomy_dl_call,
+                stdout = log_file,
+                stderr = log_file
+            )
+
+            # @KRAKEN2_BUILD@ --build --db $DBNAME
+            db_build_call = [
+                *snakemake.params["kraken2_build"].split(" "),
+                "--build",
+                "--db", kraken_dir,
+                "--max-db-size", str(max_db_size)]
+
+            logger.info(
+                "Building database with size-limit...")
+                
+            logger.info(db_build_call)
+            
+            subprocess.run(
+                db_build_call,
+                stdout = log_file,
+                stderr = log_file
+            )
+
+            # After building, the taxonomy subdir only takes up space.
+            shutil.rmtree(os.path.join(kraken_dir, "taxonomy"))
+
+        else:
+            logger.info(
+                "No downsampling requested, taking downloaded dir as is.")
+
+            tar_out.extractall(path = kraken_dir)
+
+            logger.info(
+                f"Download successfull, downloaded kraken2 files to "
+                f"{kraken_dir}.")
+
+        logger.info("Done with kraken_db.")
+
+
+    except Exception as e:
+        logger.error(e)
+        sys.exit(1)
+        
